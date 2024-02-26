dfeb9ad14e0879df910406f40cf68bc54ed9318f
Matt Watson <mwatson@renci.org>
2024-02-26 10:32:09 -0500

    fetch nc superfund sites

--- src/context/data-context.js
+++ src/context/data-context.js
@@ -1,5 +1,6 @@
 import { createContext, useContext, useState } from 'react'
 import PropTypes from 'prop-types'
+import axios from 'axios'
 import testData from '@content/test-data-v0.json'
 import { useQuery } from '@tanstack/react-query'
 
@@ -9,25 +10,28 @@ const DataContext = createContext({ })
 
 export const useData = () => useContext(DataContext)
 
+const fetchSuperfundSites = () =>
+  Promise.all([
+    axios('https://enviroscan-drf.renci.org/drf/api/nc_superfund_sites/?page=1'),
+    axios('https://enviroscan-drf.renci.org/drf/api/nc_superfund_sites/?page=2'),
+    axios('https://enviroscan-drf.renci.org/drf/api/nc_superfund_sites/?page=3'),
+    axios('https://enviroscan-drf.renci.org/drf/api/nc_superfund_sites/?page=4'),
+    axios('https://enviroscan-drf.renci.org/drf/api/nc_superfund_sites/?page=5'),
+  ])
+    .then(responses => responses.map(r => r.data.results))
+    .then(data => data.reduce((geojson, d) => {
+      geojson.features = geojson.features.concat(d.features)
+      return geojson
+    }, { type: 'FeatureCollection', features: [] }))
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => [])
+
 export const DataProvider = ({ children }) => {
   const [sampleData, ] = useState(testData)
-  const { /*isPending, error,*/ data: esData } = useQuery({
+  const { isPending, error, data: esData } = useQuery({
     queryKey: ['nc_superfund_sites'],
-    queryFn: () =>
-      Promise.all([
-        fetch('http://enviroscan-drf.renci.org/drf/api/nc_superfund_sites/?page=1'),
-        fetch('http://enviroscan-drf.renci.org/drf/api/nc_superfund_sites/?page=2'),
-        fetch('http://enviroscan-drf.renci.org/drf/api/nc_superfund_sites/?page=3'),
-        fetch('http://enviroscan-drf.renci.org/drf/api/nc_superfund_sites/?page=4'),
-        fetch('http://enviroscan-drf.renci.org/drf/api/nc_superfund_sites/?page=5'),
-      ])
-        .then(responses => {
-          const combined = [...responses.map(res => res.data)]
-          console.log(combined)
-          return combined
-        })
-        .catch(console.error)
-        .finally(() => []),
+    queryFn: fetchSuperfundSites,
   })
 
   console.log(esData)
