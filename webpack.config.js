d655006a66ba81c2b7a6aa922dc11036f390cad3
Matt Watson <mwatson@renci.org>
2024-02-27 17:29:51 -0500

    add images dir alias

--- webpack.config.js
+++ webpack.config.js
@@ -97,6 +97,7 @@ module.exports = {
       '@content': path.resolve(__dirname, 'src/content/'),
       '@context': path.resolve(__dirname, 'src/context/'),
       '@hooks': path.resolve(__dirname, 'src/hooks/'),
+      '@images': path.resolve(__dirname, 'src/images/'),
     }
   },
 
