fda83147c7a058f9484673aeffaf2aa808d56e0e
Matt Watson <mwatson@renci.org>
2024-03-15 11:39:49 -0400

    refactor beacon

--- src/components/map/layers/beacon-layer.js
+++ src/components/map/layers/beacon-layer.js
@@ -1,46 +1,55 @@
-import { useCallback } from 'react'
+import { memo, useCallback } from 'react'
 import PropTypes from 'prop-types'
 import { Marker } from 'react-map-gl'
 
-export const BeaconLayer = ({ longitude, latitude }) => {
-  const Beacon = useCallback(() => {
-    return (
-      <svg
-        height={ 48 }
-        width={ 48 }
-        viewBox="0 0 48 48"
-        style={{
-          fill: '#ff000022',
-          stroke: 'none',
-          zIndex: 9999,
-        }}
+export const Beacon = memo(function beaconSvg() {
+  return (
+    <svg
+      height={ 48 }
+      width={ 48 }
+      viewBox="0 0 48 48"
+      style={{
+        fill: '#ff000022',
+        stroke: 'none',
+        zIndex: 9999,
+      }}
+    >
+      <circle
+        cx="24" cy="27" r="2"
+        fill="#ff0000"
+        stroke="#ff0000"
+        strokeWidth="1"
       >
-        <circle
-          cx="24" cy="27" r="2"
-          fill="none"
-          stroke="#ff0000"
-          strokeWidth="2"
-        >
-          <animate
-            attributeName="r" 
-            begin="0s"
-            values="2;22"
-            keyTimes="0;1"
-            dur="1s"
-            repeatCount="indefinite"
-          />
-          <animate
-            attributeName="opacity"
-            begin="0s"
-            dur="1s"
-            repeatCount="indefinite"
-            from="100%"
-            to="0%"
-          />
-        </circle>
-      </svg>
-    )
-  }, [])
+        <animate
+          attributeName="r" 
+          begin="0s"
+          values="2;22"
+          keyTimes="0;1"
+          dur="1s"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="opacity"
+          begin="0s"
+          dur="1s"
+          repeatCount="indefinite"
+          from="100%"
+          to="0%"
+        />
+        <animate
+          attributeName="fill-opacity"
+          begin="0s"
+          dur="1s"
+          repeatCount="indefinite"
+          from="100%"
+          to="0%"
+        />
+      </circle>
+    </svg>
+  )
+})
+
+export const BeaconLayer = ({ longitude, latitude }) => {
 
   return (
     <Marker
